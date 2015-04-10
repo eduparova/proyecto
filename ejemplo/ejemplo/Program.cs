312115afbf33713887efd7aded0a3ecbc58cf82e
eduparova <eduparova@hotmail.com>
2015-04-10 17:13:02 -0500

    Base del proyecto

---
+++ ejemplo/ejemplo/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Eduardo
+ * Date: 10/04/2015
+ * Time: 17:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ejemplo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
